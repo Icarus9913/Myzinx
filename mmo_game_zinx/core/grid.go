2530f2472314952fd970b91e4809af2aa60e260c
Icarus <icaruswu66@qq.com>
2020-12-14 22:32:37 +0800

    ..

--- mmo_game_zinx/core/grid.go
+++ mmo_game_zinx/core/grid.go
@@ -1,6 +1,9 @@
 package core
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 /*
 	一个AOI地图中的格子类型
@@ -36,8 +39,33 @@ func NewGrid(gID,minX,MaxX,minY,maxY int) *Grid {
 }
 
 
-//
-//
-//
-//
-//
+//给格子添加一个玩家
+func (g *Grid) Add(playerID int) {
+	g.pIDLock.Lock()
+	defer g.pIDLock.Unlock()
+	g.playerIDs[playerID] = true
+}
+
+//从格子中删除一个玩家
+func (g *Grid) Remove(playerID int) {
+	g.pIDLock.Lock()
+	defer g.pIDLock.Unlock()
+	delete(g.playerIDs, playerID)
+}
+
+//得到当前格子中所有的玩家ID
+func (g *Grid) GetPlayerIDs() (playerIDs []int) {
+	g.pIDLock.RLock()
+	defer g.pIDLock.RUnlock()
+
+	for k, _ := range g.playerIDs {
+		playerIDs = append(playerIDs, k)
+	}
+	return
+}
+
+//调试使用-打印出格子的基本信息
+func (g *Grid) String() string {
+	return fmt.Sprintf("Grid id:%d, minX:%d, maxX:%d, minY:%d, maxY:%d, playerIDs:%v",
+		g.GID, g.MinX, g.MaxX, g.MinY, g.MaxY, g.playerIDs)
+}
