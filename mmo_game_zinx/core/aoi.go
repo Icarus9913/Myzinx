edd8f8147873cfc9cc3236aac974a1ac7f00e2d4
Icarus <icaruswu66@qq.com>
2020-12-16 22:50:49 +0800

    ..

--- mmo_game_zinx/core/aoi.go
+++ mmo_game_zinx/core/aoi.go
@@ -69,7 +69,7 @@ func (m *AOIManager) gridLength() int {
 func (m *AOIManager) String() string {
 	//打印AOIManager信息
 	s := fmt.Sprintf("AOIManager:\n MinX:%d, MaxX:%d, cntsX:%d, minY:%d, maxY:%d, cntsY:%d\n Grids in AOIManager:\n",
-		m.MinX,m.MaxX,m.CntsX,m.MinY,m.MaxY,m.CntsY)
+		m.MinX, m.MaxX, m.CntsX, m.MinY, m.MaxY, m.CntsY)
 
 	//打印全部格子信息
 	for _, grid := range m.grids {
@@ -77,4 +77,49 @@ func (m *AOIManager) String() string {
 	}
 	return s
 
+}
+
+//根据格子GID得到周边九宫格格子集合
+func (m *AOIManager) GetSurroundGridsByGid(gID int) (grids []*Grid) {
+	//判断gID是否在AOIManager中
+	if _, ok := m.grids[gID]; !ok {
+		return
+	}
+	//将当前gid本身加入九宫格切片中
+	grids = append(grids, m.grids[gID])
+
+	//通过gID的左边是否有格子，右边是否有格子
+	//需要通过gID得到当前格子x轴的编号  --idx = id % nx
+	idx := gID % m.CntsX
+
+	//判断idx编号是否左边还有格子
+	if idx > 0 {
+		grids = append(grids, m.grids[gID-1])
+	}
+	//判断idx编号是否右边还有格子
+	if idx < m.CntsX-1 {
+		grids = append(grids,m.grids[gID+1])
+	}
+
+	//将x轴当前的格子都取出，进行遍历，再分别得到每个格子上下是否还有格子
+	//得到当前x轴格子的ID集合
+	gidsX := make([]int,0,len(grids))
+	for _,v:=range grids{
+		gidsX = append(gidsX,v.GID)
+	}
+
+	//遍历gidsX集合中每个格子的gid
+	for _,v := range gidsX{
+		//得到当前格子id的y轴的编号  idy = id / ny
+		idy := v / m.CntsY
+		//gid 上边是否还有格子
+		if idy >0{
+			grids = append(grids,m.grids[v-m.CntsX])
+		}
+		//gid 下边是否还有格子
+		if idy< m.CntsY-1{
+			grids = append(grids,m.grids[v+m.CntsX])
+		}
+	}
+	return
 }
